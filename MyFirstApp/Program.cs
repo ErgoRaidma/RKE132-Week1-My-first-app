6ed0986c7742fcf0cb10bf12722ad5a8d841b1f7
Ergo <ergo.raidma@tktk.ee>
2024-02-04 10:19:47 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajalt sisestada oma nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");//Output
+//string on sõne
+string userName = Console.ReadLine();//Input
+
+Console.WriteLine("Hello" + ", " + userName + "!");//Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName}!");//Input
